ae072643b19d35137b3b59aa036d1212b6db1ee6
surya200317 <93912565+surya200317@users.noreply.github.com>
2021-12-02 23:15:39 +0530

    Add files via upload

---
+++ switch case 2.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+	int week;
+	printf("enter the week no:");
+	scanf("%d",&week);
+	switch(week)
+	{
+	case 1:
+	printf("Monday");
+	break;
+    case 2:
+	printf("Tuesday");
+	break;
+	case 3:
+	printf("Wednesday");
+	break;
+	case 4:
+	printf("Thursday");
+	break;
+	case 5:
+	printf("Thursday");
+	break;
+	case 6:
+	printf("Thursday");
+	break;
+	case 7:
+	printf("Thursday");
+	break;
+	default:
+		printf("error");
+		
+	}
+
+	return 0;
+	
+	
+}
