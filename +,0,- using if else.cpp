d33fef3105db558c0a31dd6d888d634269152790
surya200317 <93912565+surya200317@users.noreply.github.com>
2021-12-06 16:39:25 +0530

    Add files via upload

---
+++ +,0,- using if else.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(n>0)
+	{
+		printf("it is a positive numvber");
+	}
+	else if(n=0)
+	{
+		printf("the number entered is zero");
+		
+	}
+	else
+	{
+		printf("the number is neg");
+	}
+return 0;
+}
