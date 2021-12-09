84ec7f404c651481eaa870cdc6558935e716c733
surya200317 <93912565+surya200317@users.noreply.github.com>
2021-12-09 10:37:38 +0530

    Add files via upload

---
+++ goto in switch case.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int sk; 
+float a,b,c,d,f,g,h;
+printf("enter your choice");
+scanf("%d",&sk);
+a=4;
+b=3;
+
+switch(sk)
+
+{
+add:
+case 1:
+c=a+b;
+printf("%f",c);
+break;
+
+case 2:
+d=a-b;
+printf("%f",d);
+break;
+
+case 3:
+f=a/b;
+printf("%.1f",f);
+break;
+
+case 4:
+h=a*b;
+printf("%f",h);
+break;
+
+case 5:
+goto add;
+break;
+
+default:
+	printf("please enter the correct value");
+	return 0;
+	
+}
+}
